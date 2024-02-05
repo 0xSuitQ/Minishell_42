7ed71dc276d27d66ef64544954199e7907ca9fbe
Nikita Androsov <androsov@Nikitas-MacBook-Air.local>
2024-02-05 17:55:48 +0100

    lexer in work

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -1,5 +1,25 @@
-#include "../includes/lexer.h"
-#include "../includes/parser.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/05 13:33:02 by psimcak           #+#    #+#             */
+/*   Updated: 2024/02/05 17:53:48 by psimcak          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+t_lexer	*get_last_node(t_lexer *list_head)
+{
+	if (list_head == NULL)
+		return (NULL);
+	while (list_head->next)
+		list_head = list_head->next;
+	return (list_head);
+}
 
 int	substring_counter(char *str)
 {
@@ -20,118 +40,166 @@ int	substring_counter(char *str)
 	return (counter);
 }
 
-void	*create_node(t_lexer *token, int index, t_token tok, char *str) {
-    token->i = index;
-	if (token)
-		token->token = tok;
-	else
-		token->token = NULL;
+void	create_node(t_lexer **list, int index, t_token_list tok, char *str)
+{
+	t_lexer	*new_node;
+	
+	new_node = malloc(sizeof(t_lexer));
+	if (!new_node)
+		return ;
+	printf("test: %d\n", index);
+	new_node->i = index;
+	new_node->token = tok;
 	if (str)
-    	token->str = ft_strdup(str); // TODO: mind to freeing it
+		new_node->str = ft_strdup(str); // TODO: mind to freeing it
 	else
-		token->str = NULL;
-    token->next = NULL;
-    token->prev = NULL;
+		new_node->str = NULL;
+	if (index == 0)
+	{
+		*list = new_node;
+		(*list)->next = NULL;
+		(*list)->prev = NULL;
+	}
+	else
+	{
+		new_node->prev = get_last_node(*list);
+		new_node->prev->next = new_node;
+		new_node->next = NULL;
+	}
 }
 
-t_lexer	*tokenize(char **words, char *str)
+void	tokenize(char **words, t_lexer	**lexer_list)
 {
-	t_lexer		*lexer_list;
-	static void	*special_char[5][2] = {
-		{"|", PIPE},
+	static t_token_matrix tokens[] = {
+		{"|", PIPE},	
 		{"<", LESS},
 		{"<<", LESS_LESS},
 		{">", GREAT},
 		{">>", GREAT_GREAT}
 	};
-	static int	i = 0;
+	static int	i = -1;
+	static int	j;
+	static int	found;
 
-	lexer_list = malloc(sizeof(t_lexer) * (substring_counter(str) + 1));
-	// TODO Error handling for malloc
-	
-	while 
+	while (words[++i])
+	{
+		j = -1;
+		found = 0;
+		while(++j < TOKEN_NUM)
+		{
+			if (ft_strcmp(words[i], tokens[j].str_symbol) == 0)
+			{
+				create_node(lexer_list, i, tokens[j].type, NULL);
+				found = 1;
+
+			}
+		}
+		if (!found)
+			create_node(lexer_list, i, 0, words[i]);
+	}
 }
 
-t_lexer	*tokenize(char **words, char *str)
+/*t_lexer	*tokenize(char **words, char *str)
 {
-	t_lexer	*tokens;
+	t_lexer	*lexer_list;
 	int		i;
 
 	i = 0;
-	tokens = malloc(sizeof(t_lexer) * (substring_counter(str) + 1));
-	// TODO Error handling for malloc
+	
 	while (words[i])
 	{
 		if (strcmp(words[i], "|") == 0)
 		{
-			create_node(&tokens[i], i, PIPE, NULL); // Not sure if we have to send &tokens[i] in there
+			create_node(&lexer_list, i, PIPE, NULL); // Not sure if we have to send &tokens[i] in there
 		}
 		else if (strcmp(words[i], "<") == 0)
 		{
-			create_node(&tokens[i], i, LESS, NULL);
+			create_node(&lexer_list, i, LESS, NULL);
 		}
 		else if (strcmp(words[i], "<<") == 0)
 		{
-			create_node(&tokens[i], i, LESS_LESS, NULL);
+			create_node(&lexer_list, i, LESS_LESS, NULL);
 		}
 		else if (strcmp(words[i], ">") == 0)
 		{
-			create_node(&tokens[i], i, GREAT, NULL);
+			create_node(&lexer_list, i, GREAT, NULL);
 		}
 		else if (strcmp(words[i], ">>") == 0)
 		{
-			create_node(&tokens[i], i, GREAT_GREAT, NULL);
+			create_node(&lexer_list, i, GREAT_GREAT, NULL);
 		}
 		if (i == 0)
 			continue;
 		else
-			token_append(); // TODO
+			create_node(lexer_list, i, NULL, words[i]); // TODO
 		i++;
 	}
-	return (tokens);
-}
+	return (lexer_list);
+}*/
 
-char	**split_by_whitespace(char *str)
+char	**split_by_whitespace(char *input)
 {
 	char	**words;
 	int		i;
 	int		j;
 
 	i = 0;
-	words = malloc(sizeof(char *) * substring_counter(str) + 1);
-	while (*str)
+	words = malloc(sizeof(char *) * substring_counter(input) + 1);
+  if (!words)
+    return (NULL);
+	while (*input)
 	{
-		while (*str && ((*str >= 9 && *str <= 13) || *str == 32))
-			str++;
-		if (*str)
-			words[i++] = str_sepdup(str);
-		while (*str && !((*str >= 9 && *str <= 13) || *str == 32))
-			str++;
+		while (*input && ((*input >= 9 && *input <= 13) || *input == 32))
+			input++;
+		if (*input)
+			words[i++] = str_sepdup(input);
+		while (*input && !((*input >= 9 && *input <= 13) || *input == 32))
+			input++;
 	}
 	words[i] = 0;
 	return (words);
 }
 
-t_lexer *lexer(char *str)
+int	size_of_2d(char **words)
 {
-	t_lexer	*tokens;
-	char	**words;
-	int		i;
+	static int	count = 0;
+
+	while (words[count])
+		count++;
+	return (count);
+}
+
+t_lexer *lexer(char *input)
+{
+	char		**words;
+	int			i;
+	t_lexer		*lexer_list;
 
 	i = 0;
-	words = split_by_whitespace(str);
-	//tokenizer
-	
-	return (tokens);
+	lexer_list = NULL;
+	words = split_by_whitespace(input);
+	tokenize(words, &lexer_list); // TODO free
+
+	// TEST
+	while (lexer_list)
+	{
+		printf("str of the lexer list number %d: %s\n", lexer_list->i, lexer_list->str);
+		printf("token of the lexer list number %d: %u\n", lexer_list->i, lexer_list->token);
+		//if (lexer_list[i].token == GREAT)
+		//	write(1, "TEST CHECK\n", 12);
+		printf("------------------\n");
+		lexer_list = lexer_list->next;
+	}
+	return (lexer_list);
 }
 
 int	main()
 {
-	char	*str;
+	char	*input;
 
-	str = "ls -l > file.txt";
+	input = "ls -l > file.txt";
 
-	lexer(str);
+	lexer(input);
 
 	return 0;
 }
