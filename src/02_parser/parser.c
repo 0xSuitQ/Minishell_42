2b0644e3df85b8eb9bcbfb00df689d2c855bf7e3
Petr Simcak <psimcak@c1r1s1.42prague.com>
2024-02-19 19:43:20 +0100

    parser mainly - second one

--- src/02_parser/parser.c
+++ src/02_parser/parser.c
@@ -1,278 +1,278 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   parser.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/05 19:09:30 by psimcak           #+#    #+#             */
-/*   Updated: 2024/02/19 11:09:45 by psimcak          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+// /* ************************************************************************** */
+// /*                                                                            */
+// /*                                                        :::      ::::::::   */
+// /*   parser.c                                           :+:      :+:    :+:   */
+// /*                                                    +:+ +:+         +:+     */
+// /*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
+// /*                                                +#+#+#+#+#+   +#+           */
+// /*   Created: 2024/02/05 19:09:30 by psimcak           #+#    #+#             */
+// /*   Updated: 2024/02/19 16:53:12 by psimcak          ###   ########.fr       */
+// /*                                                                            */
+// /* ************************************************************************** */
 
-#include "../../includes/minishell.h"
+// #include "../../includes/minishell.h"
 
-char	*token_to_str(t_lexer *token)
-{
-	int						i;
-	static t_token_matrix	tokens[] = {
-	{"|", PIPE},
-	{"<", LESS}, {"<<", LESS_LESS},
-	{">", GREAT}, {">>", GREAT_GREAT}
-	};
+// char	*token_to_str(t_lexer *token)
+// {
+// 	int						i;
+// 	static t_token_matrix	tokens[] = {
+// 	{"|", PIPE},
+// 	{"<", LESS}, {"<<", LESS_LESS},
+// 	{">", GREAT}, {">>", GREAT_GREAT}
+// 	};
 
-	i = -1;
-	while (++i < TOKEN_NUM)
-		if (token && token->token == tokens[i].type)
-			return (tokens[i].str_sym);
-	return ("newline"); // if !token
-}
+// 	i = -1;
+// 	while (++i < TOKEN_NUM)
+// 		if (token && token->token == tokens[i].type)
+// 			return (tokens[i].str_sym);
+// 	return ("newline"); // if !token
+// }
 
-int	count_args(t_lexer *lexer_list)
-{
-	t_lexer	*tmp;
-	int		i;
+// int	count_args(t_lexer *lexer_list)
+// {
+// 	t_lexer	*tmp;
+// 	int		i;
 
-	i = 0;
-	tmp = lexer_list;
-	while (tmp && tmp->token != PIPE)
-	{
-		if (tmp->index >= 0)
-			i++;
-		tmp = tmp->next;
-	}
-	return (i);
-}
+// 	i = 0;
+// 	tmp = lexer_list;
+// 	while (tmp && tmp->token != PIPE)
+// 	{
+// 		if (tmp->index >= 0)
+// 			i++;
+// 		tmp = tmp->next;
+// 	}
+// 	return (i);
+// }
 
-t_lexer	*append_redirection(t_simple_cmd **cmd, t_lexer *arg)
-{
-	t_lexer	*new_arg;
-	t_lexer	*last;
+// t_lexer	*append_redirection(t_simple_cmd **cmd, t_lexer *arg)
+// {
+// 	t_lexer	*new_arg;
+// 	t_lexer	*last;
 	
-	new_arg = malloc(sizeof(t_lexer));
-	if (!new_arg)
-		return (NULL);
-	*new_arg = *arg;
-	new_arg->next = NULL;
-	new_arg->prev = NULL;
-	if (!(*cmd)->lexer_list)
-		(*cmd)->lexer_list = new_arg;
-	else
-	{
-		last = get_last_node((*cmd)->lexer_list);
-		last->next = new_arg;
-		new_arg->prev = last;
-	}
-	new_arg->flag = INVISIBLE;
-	return (new_arg);
-}
+// 	new_arg = malloc(sizeof(t_lexer));
+// 	if (!new_arg)
+// 		return (NULL);
+// 	*new_arg = *arg;
+// 	new_arg->next = NULL;
+// 	new_arg->prev = NULL;
+// 	if (!(*cmd)->lexer_list)
+// 		(*cmd)->lexer_list = new_arg;
+// 	else
+// 	{
+// 		last = get_last_node((*cmd)->lexer_list);
+// 		last->next = new_arg;
+// 		new_arg->prev = last;
+// 	}
+// 	// new_arg->flag = INVISIBLE;
+// 	return (new_arg);
+// }
 
-void	first_node_not_pipe(t_lexer *lex_head)
-{
-	if (lex_head->token == PIPE)
-		ft_putstr_fd_exit("syntax error near unexpected token `|'\n",
-		STDOUT, 0);
-} // check for consecutive pipes '| |'
+// void	first_node_not_pipe(t_lexer *lex_head)
+// {
+// 	if (lex_head->token == PIPE)
+// 		ft_putstr_fd_exit("syntax error near unexpected token `|'\n",
+// 		STDOUT, 0);
+// } // check for consecutive pipes '| |'
 
-void	validate_and_append_redirection(t_simple_cmd **cmd, t_lexer ***current_lexer)
-{
-	t_lexer	*the_arg;
-	t_lexer	*next_arg;
+// void	validate_and_append_redirection(t_simple_cmd **cmd, t_lexer ***current_lexer)
+// {
+// 	t_lexer	*the_arg;
+// 	t_lexer	*next_arg;
 
-	if (!(**current_lexer) || !(*cmd))
-		return ;
-	the_arg = **current_lexer;
-	next_arg = the_arg->next;
+// 	if (!(**current_lexer) || !(*cmd))
+// 		return ;
+// 	the_arg = **current_lexer;
+// 	next_arg = the_arg->next;
 
-	if ((!next_arg || next_arg->token != 0) && the_arg)
-	{
-		ft_printf("syntax error near unexpected token `%s'", token_to_str(next_arg));
-		ft_putstr_fd_exit("\n", STDOUT, 0);
-	}
-	if (the_arg->token == LESS)
-	{
-		if (access(next_arg->sub_str, F_OK) == -1)
-		{
-			ft_printf("no such file or directory: %s\\n", next_arg->sub_str);
-			return; // Exit on error for non-existing file
-		}
-	}
-	the_arg = append_redirection(cmd, the_arg);
-	if (next_arg)
-	{
-		next_arg = append_redirection(cmd, next_arg);
-		**current_lexer = next_arg; // Move lexer pointer forward
-		// arent we moving the pointer twice? OR isnt it unnecessary?
-	}
-}
+// 	if ((!next_arg || next_arg->token != 0) && the_arg)
+// 	{
+// 		ft_printf("syntax error near unexpected token `%s'", token_to_str(next_arg));
+// 		ft_putstr_fd_exit("\n", STDOUT, 0);
+// 	}
+// 	if (the_arg->token == LESS)
+// 	{
+// 		if (access(next_arg->sub_str, F_OK) == -1)
+// 		{
+// 			ft_printf("no such file or directory: %s\\n", next_arg->sub_str);
+// 			return; // Exit on error for non-existing file
+// 		}
+// 	}
+// 	the_arg = append_redirection(cmd, the_arg);
+// 	if (next_arg)
+// 	{
+// 		next_arg = append_redirection(cmd, next_arg);
+// 		**current_lexer = next_arg; // Move lexer pointer forward
+// 		// arent we moving the pointer twice? OR isnt it unnecessary?
+// 	}
+// }
 
-static void	init_first_scmd(t_simple_cmd ***list, t_simple_cmd *new_node)
-{
-	**list = new_node;
-	(**list)->prev = NULL;
-}
+// static void	init_first_scmd(t_simple_cmd ***list, t_simple_cmd *new_node)
+// {
+// 	**list = new_node;
+// 	(**list)->prev = NULL;
+// }
 
-t_simple_cmd	*get_last_cmd_node(t_simple_cmd ***lexer_list)
-{
-	if ((**lexer_list) == NULL)
-		return (NULL);
-	while ((**lexer_list)->next)
-		**lexer_list = (**lexer_list)->next;
-	return ((**lexer_list));
-}
+// t_simple_cmd	*get_last_cmd_node(t_simple_cmd ***lexer_list)
+// {
+// 	if ((**lexer_list) == NULL)
+// 		return (NULL);
+// 	while ((**lexer_list)->next)
+// 		**lexer_list = (**lexer_list)->next;
+// 	return ((**lexer_list));
+// }
 
-static void	connect_node_to_list(t_simple_cmd ***list, t_simple_cmd **new_node)
-{
-	(*new_node)->prev = get_last_cmd_node(list);
-	(**list)->next = *new_node;
-	(*new_node)->next = NULL;
-}
+// static void	connect_node_to_list(t_simple_cmd ***list, t_simple_cmd **new_node)
+// {
+// 	(*new_node)->prev = get_last_cmd_node(list);
+// 	(**list)->next = *new_node;
+// 	(*new_node)->next = NULL;
+// }
 
-void	init_simple_cmd(t_simple_cmd **s_cmd_list, t_lexer *lexer_list)
-{
-	t_simple_cmd	*new_node;
-	int				arg_size;
+// void	init_simple_cmd(t_simple_cmd **s_cmd_list, t_lexer *lexer_list)
+// {
+// 	t_simple_cmd	*new_node;
+// 	int				arg_size;
 
-	arg_size = count_args(lexer_list);
-	new_node = malloc(sizeof(t_simple_cmd));
-	if (!new_node)
-		return ;
-	new_node->str = malloc((arg_size + 1) * sizeof(char *));
-	if (!new_node->str)
-	{
-		free(new_node);
-		return ;
-	}
-	if (*s_cmd_list == NULL)
-		init_first_scmd(&s_cmd_list, new_node);
-	else
-		connect_node_to_list(&s_cmd_list, &new_node);
-	new_node->lexer_list = NULL;
-	new_node->next = NULL;
-}
+// 	arg_size = count_args(lexer_list);
+// 	new_node = malloc(sizeof(t_simple_cmd));
+// 	if (!new_node)
+// 		return ;
+// 	new_node->str = malloc((arg_size + 1) * sizeof(char *));
+// 	if (!new_node->str)
+// 	{
+// 		free(new_node);
+// 		return ;
+// 	}
+// 	if (*s_cmd_list == NULL)
+// 		init_first_scmd(&s_cmd_list, new_node);
+// 	else
+// 		connect_node_to_list(&s_cmd_list, &new_node);
+// 	new_node->lexer_list = NULL;
+// 	new_node->next = NULL;
+// }
 
-void	check_pipes(t_simple_cmd **cmd, t_lexer *lexer_list)
-{
-	while(lexer_list)
-	{
-		if (lexer_list->token == PIPE)
-			init_simple_cmd(cmd, lexer_list->next); // Not to start counting from pipe
-		lexer_list = lexer_list->next;
-	}
-}
+// void	check_pipes(t_simple_cmd **cmd, t_lexer *lexer_list)
+// {
+// 	while(lexer_list)
+// 	{
+// 		if (lexer_list->token == PIPE)
+// 			init_simple_cmd(cmd, lexer_list->next); // Not to start counting from pipe
+// 		lexer_list = lexer_list->next;
+// 	}
+// }
 
-void	check_redirection(t_simple_cmd **cmd, t_lexer **lexer_list)
-{
-	while (*lexer_list)
-	{
-		if ((*lexer_list)->token && (*lexer_list)->token != PIPE)
-			validate_and_append_redirection(cmd, &lexer_list);
-		else if ((*lexer_list)->token == PIPE)
-			*cmd = (*cmd)->next;
-		*lexer_list = (*lexer_list)->next;
-	}
-}
+// void	check_redirection(t_simple_cmd **cmd, t_lexer **lexer_list)
+// {
+// 	while (*lexer_list)
+// 	{
+// 		if ((*lexer_list)->token && (*lexer_list)->token != PIPE)
+// 			validate_and_append_redirection(cmd, &lexer_list);
+// 		else if ((*lexer_list)->token == PIPE)
+// 			*cmd = (*cmd)->next;
+// 		*lexer_list = (*lexer_list)->next;
+// 	}
+// }
 
-void	check_cmds(t_simple_cmd **cmd, t_lexer *lexer_list)
-{
-	t_simple_cmd *head;
-	int i;
+// void	check_cmds(t_simple_cmd **cmd, t_lexer *lexer_list)
+// {
+// 	t_simple_cmd *head;
+// 	int i;
 
-	head = *cmd;
-	i = -1;
-	while (lexer_list)
-	{
-		if (lexer_list->token == PIPE)
-		{
-			(*cmd)->str[i] = NULL;
-			*cmd = (*cmd)->next;
-			printf("cmd pointer: %p\n", *cmd); // delete
-			i = -1;
-		}
-		else if (lexer_list->flag == INVISIBLE)
-			lexer_list = lexer_list->next;
-		(*cmd)->str[++i] = ft_strdup(lexer_list->sub_str);
-		lexer_list = lexer_list->next;
-	}
-	if (i == -1)
-		i++;
-	(*cmd)->str[i] = NULL;
+// 	head = *cmd;
+// 	i = -1;
+// 	while (lexer_list)
+// 	{
+// 		if (lexer_list->token == PIPE)
+// 		{
+// 			(*cmd)->str[i] = NULL;
+// 			*cmd = (*cmd)->next;
+// 			printf("cmd pointer: %p\n", *cmd); // delete
+// 			i = -1;
+// 		}
+// 		else if (lexer_list->flag == INVISIBLE)
+// 			lexer_list = lexer_list->next;
+// 		(*cmd)->str[++i] = ft_strdup(lexer_list->sub_str);
+// 		lexer_list = lexer_list->next;
+// 	}
+// 	if (i == -1)
+// 		i++;
+// 	(*cmd)->str[i] = NULL;
 	
-	*cmd = head;
-	int j;
-	while (*cmd)
-	{
-		j = 0;
-		while((*cmd)->str[j])
-			printf("%s ", (*cmd)->str[j++]);
-		printf("\n");
-		*cmd = (*cmd)->next;
-	}
-	*cmd = head;
-}
+// 	*cmd = head;
+// 	int j;
+// 	while (*cmd)
+// 	{
+// 		j = 0;
+// 		while((*cmd)->str[j])
+// 			printf("%s ", (*cmd)->str[j++]);
+// 		printf("\n");
+// 		*cmd = (*cmd)->next;
+// 	}
+// 	*cmd = head;
+// }
 
-void check_redirections_tokens(t_simple_cmd *cmd) 
-{
-	while (cmd != NULL)
-	{
-		while (cmd->lexer_list != NULL)
-		{
-			printf("Redirection token: %s\n", cmd->lexer_list->sub_str);
-			cmd->lexer_list = cmd->lexer_list->next;
-		}
-		cmd = cmd->next;
-	}
-}
+// void check_redirections_tokens(t_simple_cmd *cmd) 
+// {
+// 	while (cmd != NULL)
+// 	{
+// 		while (cmd->lexer_list != NULL)
+// 		{
+// 			printf("Redirection token: %s\n", cmd->lexer_list->sub_str);
+// 			cmd->lexer_list = cmd->lexer_list->next;
+// 		}
+// 		cmd = cmd->next;
+// 	}
+// }
 
-void print_simple_cmds(t_simple_cmd *cmd)
-{
-	t_simple_cmd *head = cmd;
+// void print_simple_cmds(t_simple_cmd *cmd)
+// {
+// 	t_simple_cmd *head = cmd;
 
-	write(1, "\n", 1);
-	while (cmd != NULL)
-	{
-		int i = -1;
-		while (cmd->str[++i] != NULL)
-			printf("%s ", cmd->str[i]);
-		cmd = cmd->next;
-	}
-	write(1, "\n", 1);
-	check_redirections_tokens(head);
-}
+// 	write(1, "\n", 1);
+// 	while (cmd != NULL)
+// 	{
+// 		int i = -1;
+// 		while (cmd->str[++i] != NULL)
+// 			printf("%s ", cmd->str[i]);
+// 		cmd = cmd->next;
+// 	}
+// 	write(1, "\n", 1);
+// 	check_redirections_tokens(head);
+// }
 
-void	parser(t_main_tools *tools)
-{
-	t_lexer			*lexer_list;
-	t_simple_cmd	*s_cmd_list;
-	t_lexer			*lex_head;
-	t_simple_cmd 	*scmd_head;
+// void	parser(t_main_tools *tools)
+// {
+// 	t_lexer			*lexer_list;
+// 	t_simple_cmd	*s_cmd_list;
+// 	t_lexer			*lex_head;
+// 	t_simple_cmd 	*scmd_head;
 
-	lexer_list = tools->lexer_list;
-	s_cmd_list = tools->simple_cmd_list;
-	lex_head = lexer_list;
-	first_node_not_pipe(lex_head);
-	init_simple_cmd(&s_cmd_list, lexer_list);
-	scmd_head = s_cmd_list;
-	check_pipes(&s_cmd_list, lexer_list);
-	s_cmd_list = scmd_head;
-	check_redirection(&s_cmd_list, &lexer_list);
-	s_cmd_list = scmd_head;
-	lexer_list = lex_head;
-	check_cmds(&s_cmd_list, lexer_list);
-	/*
-	lexer_list = lex_head;
-	s_cmd_list = scmd_head;
-	// FREE T_LEXER LIST
-	s_cmd_list = scmd_head;
-	print_simple_cmds(s_cmd_list);
-	*/
-	// int count = 0;
-	// while (s_cmd_list)
-	// {
-	// 	count++;
-	// 	s_cmd_list = s_cmd_list->next;
-	// }
-	// printf("count: %d\n", count);
-	// print the simple commands
-	// s_cmd_list = scmd_head;
-}
+// 	lexer_list = tools->lexer_list;
+// 	s_cmd_list = tools->simple_cmd_list;
+// 	lex_head = lexer_list;
+// 	first_node_not_pipe(lex_head);
+// 	init_simple_cmd(&s_cmd_list, lexer_list);
+// 	scmd_head = s_cmd_list;
+// 	check_pipes(&s_cmd_list, lexer_list);
+// 	s_cmd_list = scmd_head;
+// 	check_redirection(&s_cmd_list, &lexer_list);
+// 	s_cmd_list = scmd_head;
+// 	lexer_list = lex_head;
+// 	check_cmds(&s_cmd_list, lexer_list);
+// 	/*
+// 	lexer_list = lex_head;
+// 	s_cmd_list = scmd_head;
+// 	// FREE T_LEXER LIST
+// 	s_cmd_list = scmd_head;
+// 	print_simple_cmds(s_cmd_list);
+// 	*/
+// 	// int count = 0;
+// 	// while (s_cmd_list)
+// 	// {
+// 	// 	count++;
+// 	// 	s_cmd_list = s_cmd_list->next;
+// 	// }
+// 	// printf("count: %d\n", count);
+// 	// print the simple commands
+// 	// s_cmd_list = scmd_head;
+// }
---
+++ src/02_parser/parser2.c
@@ -0,0 +1,276 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser2.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/19 15:03:55 by psimcak           #+#    #+#             */
+/*   Updated: 2024/02/19 19:42:04 by psimcak          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+/*
+
+*/
+char	*token_to_str(t_lexer *token)
+{
+	int						i;
+	static t_token_matrix	tokens[] = {
+	{"|", PIPE},
+	{"<", LESS}, {"<<", LESS_LESS},
+	{">", GREAT}, {">>", GREAT_GREAT}
+	};
+
+	i = -1;
+	while (++i < TOKEN_NUM)
+		if (token && token->token == tokens[i].type)
+			return (tokens[i].str_sym);
+	return ("newline"); // if !token
+}
+
+/*
+
+*/
+t_lexer	*append_redirection(t_simple_cmd **cmd, t_lexer *arg)
+{
+	t_lexer	*new_arg;
+	t_lexer	*last;
+	
+	new_arg = malloc(sizeof(t_lexer));
+	if (!new_arg)
+		return (NULL);
+	*new_arg = *arg;
+	new_arg->next = NULL;
+	new_arg->prev = NULL;
+	if (!(*cmd)->lexer_list)
+		(*cmd)->lexer_list = new_arg;
+	else
+	{
+		last = get_last_node((*cmd)->lexer_list);
+		last->next = new_arg;
+		new_arg->prev = last;
+	}
+	printf("hehehe\n");
+	return (new_arg);
+}
+
+/*
+	
+*/
+void	validate_and_append_redirection(t_simple_cmd **cmd, t_lexer **current_lexer)
+{
+	t_lexer	*the_arg;
+	t_lexer	*next_arg;
+
+	if (!(*current_lexer) || !(*cmd))
+		return ;
+	the_arg = *current_lexer;
+	next_arg = the_arg->next;
+
+	if ((!next_arg || next_arg->token != 0) && the_arg)
+	{
+		ft_printf("syntax error near unexpected token `%s'", token_to_str(next_arg));
+		ft_putstr_fd_exit("\n", STDOUT, 0);
+	}
+	if (the_arg->token == LESS)
+	{
+		if (access(next_arg->sub_str, F_OK) == -1)
+		{
+			ft_printf("no such file or directory: %s\\n", next_arg->sub_str);
+			return; // Exit on error for non-existing file
+		}
+	}
+	(*current_lexer)->flag = INVISIBLE;
+	printf("**********\n");
+	the_arg = append_redirection(cmd, the_arg);
+	printf("flag of the arg: %d\n", the_arg->flag);
+	if (next_arg)
+	{
+		next_arg = append_redirection(cmd, next_arg);
+		(*current_lexer)->flag = INVISIBLE;
+		printf("+++++++++++\n");
+		*current_lexer = next_arg; // Move lexer pointer forward
+		// arent we moving the pointer twice? OR isnt it unnecessary?
+	}
+}
+
+/*
+	@brief:
+	if the token is a redirection, it calls the append_redirection function
+	if the token is a pipe, it moves to the next node of the simple_cmd list
+	if the token is anything else (word), it moves to the next node of the list
+	of lexer_list
+*/
+void	check_redirection(t_simple_cmd **cmd, t_lexer *lexer_list)
+{
+	t_lexer	*tmp;
+
+	tmp = lexer_list;
+	printf("adress of tmp: %p\n", tmp);
+	printf("adress of lexer_list: %p\n", lexer_list);
+	while (tmp)
+	{
+		if (tmp->token && tmp->token != PIPE)
+			validate_and_append_redirection(cmd, &tmp);
+		else if (tmp->token == PIPE)
+			*cmd = (*cmd)->next;
+		tmp = tmp->next;
+	}
+}
+
+/*
+	@brief:
+	simple function count_args function counts the number of arguments in the
+	lexer_list. It is used to allocate the right amount of memory in the
+	init_simple_cmd function. It ends when it reaches the end of the list or
+	when it encounters a pipe.
+*/
+int	count_args(t_lexer *lexer_list)
+{
+	int		counter;
+
+	counter = 0;
+	while (lexer_list && lexer_list->token != PIPE)
+	{
+		if (lexer_list->index >= 0)
+			counter++;
+		lexer_list = lexer_list->next;
+	}
+	printf("count_args output: %d\n", counter);
+	return (counter);
+}
+
+/*
+	@brief:
+	get_last_cmd_node function returns the last node of the list of simple_cmd
+	structures. It uses tmp variable to go through the list without changing
+	the head of the list in the functions that call it.
+*/
+t_simple_cmd	*get_last_cmd_node(t_simple_cmd **lexer_list)
+{
+	t_simple_cmd	*tmp;
+
+	tmp = *lexer_list;
+	if (tmp == NULL)
+		return (NULL);
+	while (tmp->next)
+		tmp = tmp->next;
+	return (tmp);
+}
+
+/*
+	@brief:
+	init_first_scmd function initializes the first node of the list of
+	simple_cmd structures.
+*/
+void	init_first_scmd(t_simple_cmd **list, t_simple_cmd *new_node)
+{
+	*list = new_node;
+	(*list)->prev = NULL;
+}
+
+/*
+	@brief:
+	connect_node_to_list function connects the new_node to the list of
+	simple_cmd structures.
+*/
+void	connect_node_to_list(t_simple_cmd **list, t_simple_cmd *new_node)
+{
+	new_node->prev = get_last_cmd_node(list);
+	(*list)->next = new_node;
+}
+
+/*
+	@brief:
+	init_one_simple_cmd function initializes the simple_cmd structure. It 
+	allocates memory for the str member and sets the next and lexer_list
+	members to NULL.
+	
+	'new_node' is a structure that is added to the list of simple_cmd
+	structures. This list is used to store the simple commands that are
+	separeted by pipes.
+*/
+void	init_one_simple_cmd(t_simple_cmd **cmd_list, t_lexer *lexer_list)
+{
+	t_simple_cmd	*new_node;
+	int				arg_size;
+
+	arg_size = count_args(lexer_list);
+	new_node = malloc(sizeof(t_simple_cmd));
+	if (!new_node)
+		return ;
+	new_node->str = malloc((arg_size + 1) * sizeof(char *));
+	if (!new_node->str)
+	{
+		free(new_node);
+		return ;
+	}
+	if (*cmd_list == NULL)
+		init_first_scmd(cmd_list, new_node);
+	else
+		connect_node_to_list(cmd_list, new_node);
+	new_node->lexer_list = NULL;
+	new_node->next = NULL;
+}
+
+/*
+	@brief:
+	init_simple_cmds function initializes the list of simple_cmd structures.
+	It goes through the lexer_list and calls the init_one_simple_cmd function
+	every time it encounters a pipe.
+*/
+void	init_simple_cmds(t_simple_cmd **cmd_list, t_lexer *lexer_list)
+{
+	t_lexer	*tmp;
+
+	tmp = lexer_list;
+	if (tmp)
+		init_one_simple_cmd(cmd_list, tmp);
+	while (tmp)
+	{
+		if (tmp->token == PIPE && tmp->next)
+			init_one_simple_cmd(cmd_list, tmp->next);
+		tmp = tmp->next;
+	}
+}
+
+/*
+	@brief:
+	first_node_not_pipe function checks if the first node is a pipe. If it is,
+	it prints an error message and exits the program.
+*/
+void	first_node_not_pipe(t_lexer *lex_head)
+{
+	if (lex_head->token == PIPE)
+		ft_putstr_fd_exit("syntax error near unexpected token `|'\n",
+		STDOUT, 0);
+}
+
+/*
+	@brief:
+	parser function is the main function that calls all the other functions
+*/
+void	parser(t_main_tools *tools)
+{
+	t_lexer			*lexer_list;
+	t_simple_cmd	*s_cmd_list;
+
+	lexer_list = tools->lexer_list;
+	s_cmd_list = tools->simple_cmd_list;
+	first_node_not_pipe(lexer_list);
+	init_simple_cmds(&s_cmd_list, lexer_list);
+	check_redirection(&s_cmd_list, lexer_list);
+	
+	// delete this
+	t_lexer	*tmp = lexer_list;
+	while(tmp)
+	{
+		printf("lexer_list->flag: %d\n", tmp->flag);
+		tmp = tmp->next;
+	}
+	// WIP:
+	// check_cmds(&s_cmd_list, lexer_list);
+}
