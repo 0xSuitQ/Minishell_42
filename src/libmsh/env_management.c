22f4f2ac3e58a93fd4cebd978c37b42b6d58f456
Nikita Androsov <nandroso@c3r8s1.42prague.com>
2024-03-07 23:14:16 +0100

    fixed envp list

--- src/libmsh/env_management.c
+++ src/libmsh/env_management.c
@@ -6,7 +6,7 @@
 /*   By: nandroso <nandroso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 21:01:08 by nandroso          #+#    #+#             */
-/*   Updated: 2024/03/07 15:27:32 by nandroso         ###   ########.fr       */
+/*   Updated: 2024/03/07 18:37:15 by nandroso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,39 +15,44 @@
 void	add_env(t_main_tools *tools, char *name, char *value)
 {
 	t_env	*env;
+	t_env	*last;
 
 	env = malloc(sizeof(t_env));
 	if (env == NULL)
         return ;
 	env->name = ft_strdup(name);
-	if (!env->name)
-		write(1, "Error: env->name is NULL\n", 25);
 	env->value = ft_strdup(value);
-	if (!env->value)
-		write(1, "Error: env->value is NULL\n", 26);
 	if (tools->envp_cpy == NULL)
 	{
 		tools->envp_cpy = env;
 		tools->envp_cpy->next = NULL;
 	}
 	else
-		tools->envp_cpy->next = env;
+	{
+		last = tools->envp_cpy;
+		if (last == NULL)
+			write(1, "Error: last is NULL\n", 21);
+		while (last->next)
+			last = last->next;
+		last->next = env;
+		env->next = NULL;
+	}
 }
 
 void	copy_env(t_main_tools *tools, char **envp)
 {
 	char	**envp_cpy;
 
+	tools->envp_cpy = NULL;
 	while (*envp)
 	{
-		write(1, "we are here\n", 12);
 		envp_cpy = ft_split(*envp, '=');
 		if (!envp_cpy[0] || !envp_cpy[1])
 			write(1, "Error: envp_cpy[0] or envp_cpy[1] is NULL\n", 41);
 		add_env(tools, envp_cpy[0], envp_cpy[1]);
-		// free(envp_cpy[0]);
-		// free(envp_cpy[1]);
-		// free(envp_cpy);
+		free(envp_cpy[0]);
+		free(envp_cpy[1]);
+		free(envp_cpy);
 		envp++;
 	}
 }
