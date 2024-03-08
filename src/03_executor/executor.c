49569df4b85e955e4e099e947347f52c6f7c30d6
Nikita Androsov <nandroso@c3r8s1.42prague.com>
2024-03-08 14:53:33 +0100

    started export

--- src/03_executor/executor.c
+++ src/03_executor/executor.c
@@ -1,24 +1,24 @@
-#include "../../includes/minishell.h"
+// #include "../../includes/minishell.h"
 
-int	executor(t_main_tools *tools)
-{
-	int	fd[2];
-	int	fd_in;
+// int	executor(t_main_tools *tools)
+// {
+// 	int	fd[2];
+// 	int	fd_in;
 
-	fd_in = STDIN_FILENO;
-	while (tools->simple_cmd_list)
-	{
-		// expander
-		if (tools->simple_cmd_list->next)
-			pipe(fd);
-		heredoc(tools, tools->simple_cmd_list);
-		// create fork function
-		close(fd[1]);
+// 	fd_in = STDIN_FILENO;
+// 	while (tools->simple_cmd_list)
+// 	{
+// 		// expander
+// 		if (tools->simple_cmd_list->next)
+// 			pipe(fd);
+// 		heredoc(tools, tools->simple_cmd_list);
+// 		// create fork function
+// 		close(fd[1]);
 
-		if (tools->simple_cmd_list->heredoc_filename)
-			fd_in = fd[0];
-		if (tools->simple_cmds->next)
-			tools->simple_cmds = tools->simple_cmds->next;
+// 		if (tools->simple_cmd_list->heredoc_filename)
+// 			fd_in = fd[0];
+// 		if (tools->simple_cmds->next)
+// 			tools->simple_cmds = tools->simple_cmds->next;
 
-	}
-}
+// 	}
+// }
