71e817df742f482046e29d0e090c221e91e6eb0b
simcak <peta.simcak@gmail.com>
2024-04-09 19:56:14 +0200

    $ stuff working with edge cases (not 0)

--- src/03_executor/expander.c
+++ src/03_executor/expander.c
@@ -6,7 +6,7 @@
 /*   By: peta <peta@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 20:19:08 by psimcak           #+#    #+#             */
-/*   Updated: 2024/04/09 13:26:04 by peta             ###   ########.fr       */
+/*   Updated: 2024/04/09 19:52:52 by peta             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,6 +56,18 @@
 // 	}
 // }
 
+char	*clean_env_variable(char *str)
+{
+	int		i;
+	char	*tmp;
+
+	i = 0;
+	while (str[i] && str[i] > 'A' && str[i] < 'Z')
+		i++;
+	tmp = ft_substr(str, 0, i);
+	return (tmp);
+}
+
 /*
 	@brief:
 	- expand_dollar function expands the dollar sign and the env-variable
@@ -68,26 +80,26 @@
 void	expand_dollar(char *str)
 {
 	int		i;
-	char	*env_var;
-	char	*tmp;
-	char	*tmp2;
+	char	*env_expanded;
+	char	*result;
+	char	*tmp_env;
+	char	*pure;
 
 	i = 0;
 	if (!str[i])
 		return ;
-	tmp = ft_substr(str, 0, i);
-	tmp2 = ft_strdup(&str[i + 1]);
-	env_var = getenv(tmp2);
-	if (env_var)
+	result = ft_substr(str, 0, i);
+	pure = clean_env_variable(&str[i + 1]);
+	tmp_env = ft_strdup(&str[i + 1]);
+	env_expanded = getenv(pure);
+	if (env_expanded)
 	{
-		tmp = ft_strjoin(tmp, env_var);
-		tmp = ft_strjoin(tmp, tmp2 + ft_strlen(env_var));
-		ft_strlcpy(str, tmp, ft_strlen(tmp) + 1);
-		free(tmp);
+		result = ft_strjoin(result, env_expanded);
+		result = ft_strjoin(result, tmp_env + ft_strlen(env_expanded));
+		ft_strlcpy(str, result, ft_strlen(result) + 1);
+		free(result);
 	}
-	else
-		ft_strlcpy(str, tmp, ft_strlen(tmp) + 1);
-	free(tmp2);
+	free(tmp_env);
 }
 
 char	*handle_dollar(char *str)
@@ -104,9 +116,14 @@ char	*handle_dollar(char *str)
 			ft_strlcpy(&tmp[i], &tmp[i + 1], ft_strlen(&tmp[i + 1]) + 1);
 			i++;
 		}
+		if (tmp[i] == '$' && tmp[i + 1] == '\\')
+		{
+			i++;
+			ft_strlcpy(&tmp[i], &tmp[i + 1], ft_strlen(&tmp[i + 1]) + 1);
+		}
 		if (tmp[i] == '$')
 		{
-			expand_dollar(tmp);
+			expand_dollar(&tmp[i]);
 		}
 	}
 	return (tmp);
@@ -152,9 +169,9 @@ void	expander(t_simple_cmd *curr_simple_cmd)
 	free_arr(curr_simple_cmd->str);
 	curr_simple_cmd->str = expanded_str;
 
-	// int j = -1;
-	// printf("final: ");
-	// while (++j < i)
-	// 	printf("%s ", curr_simple_cmd->str[j]);
-	// printf("\n");
+	int j = -1;
+	printf("final: ");
+	while (++j < i)
+		printf("%s ", curr_simple_cmd->str[j]);
+	printf("\n");
 }
